91e50bb3ea16e34b0c2186663c688e76c1b79f37
billy <lee.min.seoul@gmail.com>
2025-09-03 13:21:05 -0500

    Add more cli functionality

--- mylib/logic.py
+++ mylib/logic.py
@@ -1,8 +1,15 @@
 import wikipedia
 
 
+
 def wiki(name="War Goddess", length=1):
     """Fetch wiki"""
 
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
+
+def search_wiki(name):
+    """Search Wikipedia for names"""
+
+    results = wikipedia.search(name)
+    return results
